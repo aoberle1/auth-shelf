c935cf642dfa19c105e0f030c89d5f61027d9ea2
pete5315 <124735535+pete5315@users.noreply.github.com>
2023-06-22 13:42:46 -0500

    added inputs and post function to Form component page

--- src/components/ShelfPage/ShelfPage.js
+++ src/components/ShelfPage/ShelfPage.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import Form from '../Form/Form'
 
 function ShelfPage() {
   return (
     <div className="container">
       <h2>Shelf</h2>
+      <Form />
       <p>All of the available items can be seen here.</p>
     </div>
   );
