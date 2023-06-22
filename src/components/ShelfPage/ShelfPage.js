7b0d4305fe1c26a03bbdccc4cd89f4e5c452f6bb
pete5315 <124735535+pete5315@users.noreply.github.com>
2023-06-22 12:22:04 -0700

    Update ShelfPage.js

--- src/components/ShelfPage/ShelfPage.js
+++ src/components/ShelfPage/ShelfPage.js
@@ -17,8 +17,6 @@ function ShelfPage() {
     <div className="container">
       <h2>Shelf</h2>
       <p>All of the available items can be seen here.</p>
-      <ul>
-      </ul>
     </div>
   );
 }
