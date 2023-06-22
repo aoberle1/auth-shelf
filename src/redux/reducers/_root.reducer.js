799ccfc7528e5adf928fbaa4506d2b856a5ccf7d
pete5315 <124735535+pete5315@users.noreply.github.com>
2023-06-22 12:46:34 -0700

    Update _root.reducer.js

--- src/redux/reducers/_root.reducer.js
+++ src/redux/reducers/_root.reducer.js
@@ -1,7 +1,7 @@
 import { combineReducers } from 'redux';
 import errors from './errors.reducer';
 import user from './user.reducer';
-import item from './shelf.reducer';
+import shelf from './shelf.reducer';
 
 // rootReducer is the primary reducer for our entire project
 // It bundles up all of the other reducers so our project can use them.
