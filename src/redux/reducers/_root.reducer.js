d9efa7d9712a50ab3ee12f04bb67fe195cbb4a53
pete5315 <124735535+pete5315@users.noreply.github.com>
2023-06-22 12:46:14 -0700

    Update _root.reducer.js

--- src/redux/reducers/_root.reducer.js
+++ src/redux/reducers/_root.reducer.js
@@ -10,7 +10,7 @@ import item from './shelf.reducer';
 // Lets make a bigger object for our store, with the objects from our reducers.
 // This is what we get when we use 'state' inside of 'mapStateToProps'
 const rootReducer = combineReducers({
-  item,
+  shelf,
   errors, // contains registrationMessage and loginMessage
   user, // will have an id and username if someone is logged in
 });
