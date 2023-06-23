d6116e902df077903c3445244d203920f0ee87e4
Adam Oberle <oberle.adam@gmail.com>
2023-06-23 11:57:23 -0500

    added put functionality

--- src/components/ShelfItem/ShelfItem.jsx
+++ src/components/ShelfItem/ShelfItem.jsx
@@ -1,18 +1,57 @@
+import { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 
 
 function ShelfItem({item}) {
-console.log('dfkjlsd ajfwelfjewfawd==>',item.description);
+console.log('item.description is:', item.description);
 const dispatch = useDispatch();
 const user = useSelector(store=>store.user);
+
+const [newDescription, setNewDescription] = useState(item.description);
+const [newImageUrl, setNewUrl] = useState(item.image_url);
+
+const [editOn, setEditOn] = useState(false)
+
+// const editOn = false;
+
+
+function handleEdit () {
+    setEditOn(true);
+}
 
 const handleDelete = (item) => {
     dispatch({type:'DELETE_SHELF_ITEM',
                 payload:{ item} })
 }
+
+function submitEdit () {
+    dispatch({ type: 'EDIT_ITEM', payload: {description: newDescription, image_url: newImageUrl, item_id: item.id, user_id: item.user_id}})
+    setEditOn(false)
+}
+
     return (
             <div>
             <h4>{item.description} </h4>
+            {editOn && (
+                    <form onSubmit={(e) => submitEdit(e)}>
+                    <label>Description</label>
+                    <input
+                      type="text"
+                      value={newDescription}
+                      onChange={(e) => setNewDescription(e.target.value)}
+                      required
+                    />
+                    <label>Image URL</label>
+                    <input
+                      type="text"
+                      value={newImageUrl}
+                      onChange={(e) => setNewUrl(e.target.value)}
+                      required 
+                      />
+                    <input type="submit" value="Submit"></input>
+                  </form>
+            )}
+            <button onClick={() => handleEdit()}>EDIT</button>
             
             <img src={item.image_url} />
             {/* if the user was the user that added an item, they can delete it, otherwise the button won't show */}
--- src/redux/sagas/_root.saga.js
+++ src/redux/sagas/_root.saga.js
@@ -5,6 +5,7 @@ import userSaga from './user.saga';
 import deleteSaga from './deleteShelf.saga';
 import addItemSaga from './addItem.saga';
 import shelfSaga from './shelfPage.saga'
+import editItemSaga from './editItem.saga';
 // rootSaga is the primary saga.
 // It bundles up all of the other sagas so our project can use them.
 // This is imported in index.js as rootSaga
@@ -20,5 +21,6 @@ export default function* rootSaga() {
     deleteSaga(),
     addItemSaga(),
     shelfSaga(),
+    editItemSaga(),
   ]);
 }
